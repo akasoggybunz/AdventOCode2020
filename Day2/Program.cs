35490936bad6f5ec2781a9dfc5db0517880d0dfa
Austin Foster <afoster@unionwireless.com>
2020-12-08 14:05:00 -0700

    start day 2, give readmes

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Day2
 {
@@ -6,7 +8,27 @@ namespace Day2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(" ~~~ Advent of Code Day 1!!! ~~~");
+            List<string> inputList = new List<string>();
+            int sum = 0;
+
+            // Grab Input
+            try
+            {
+                Console.WriteLine(" ~~~ Reading Expense report input file");
+
+                using StreamReader sr = new StreamReader("./input.txt");
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    inputList.Add(line);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Error: " + exception.ToString());
+            }
         }
     }
 }
